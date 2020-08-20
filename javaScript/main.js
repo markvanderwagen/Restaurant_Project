1a0740d3c6a300b26ef33c20b2c8c0b41537ed8a
markvanderwagen <65869857+markvanderwagen@users.noreply.github.com>
2020-08-21 00:45:33 +0200

    Update main.js

--- javaScript/main.js
+++ javaScript/main.js
@@ -105,30 +105,30 @@ function dropDownList() {
 
 // checking which element are in the save for later page and what to check on the main page
 function checkOnLoad() {
-  let storageArray = JSON.parse(localStorage.getItem("saveForLater"));
+  if (document.location.pathname === '/index.html') {
+    let storageArray = JSON.parse(localStorage.getItem("saveForLater"));
 
-  if (storageArray !== null) {
-    storageArray.forEach(function(item, index) { // for each loop to find index of the item
-      let itemKey = item.body;
-      let content = document.querySelectorAll('.content');
+    if (storageArray !== null) {
+      storageArray.forEach(function(item, index) { // for each loop to find index of the item
+        let itemKey = item.body;
+        let content = document.querySelectorAll('.content');
 
-        if (content[index] = itemKey) {
-          if (document.location.pathname === "/index.html") {
+        if (content[index] === itemKey) {
             // switch case to check or uncheck the inputs
-            switch (index) {
-              case 0:
-                document.getElementById("checkbox0").checked = true;
-                break;
-              case 1:
-                document.getElementById("checkbox1").checked = true;
-                break;
-              case 2:
-                document.getElementById("checkbox2").checked = true;
-                break;
-            }
+          switch (index) {
+            case 0:
+              document.getElementById("checkbox0").checked = true;
+              break;
+            case 1:
+              document.getElementById("checkbox1").checked = true;
+              break;
+            case 2:
+              document.getElementById("checkbox2").checked = true;
+              break;
           }
         }
-    });
+      });
+    }
   }
 }
 
